52a0b2049b8a1b9d81c5e8db06ce83e9f9444c4a
chucky-golden <chukschibyke92@gmail.com>
2024-08-30 12:53:00 +0100

    bids

--- src/app/bids/bid.service.ts
+++ src/app/bids/bid.service.ts
@@ -98,6 +98,22 @@ export class BidService {
       throw new NotFoundException('Bid Not Found');
     }
 
+    const creatorId = bid.job.creatorId;
+
+    const creator = await this.connection.db.collection('users').findOne(
+      { _id: creatorId },
+      { projection: { firstName: 1, lastName: 1, email: 1 } }
+    );
+
+    // If the creator is found, attach the information to the job object
+    if (creator) {
+      bid.job.creator = {
+        firstName: creator.firstName,
+        lastName: creator.lastName,
+        email: creator.email
+      };
+    }
+    
     return bid;
   }
   
